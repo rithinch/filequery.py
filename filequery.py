1925d0d79b21faa5ac5890cb7f699464bbda62d5
Rithin Chalumuri <rithin.chalumuri@pinewood.co.uk>
2019-10-06 23:13:47 +0100

    Added findall and find methods

--- filequery.py
+++ filequery.py
@@ -1,24 +1,29 @@
 import glob
 import re
 
-def file_query(folder, query, extensions=[], recursive=True):
+def findall(query, extensions=[], folders=[], recursive=True):
+    
+    matches = []
 
-    for ext in extensions:
+    for folder in folders:
+        for ext in extensions:
+            files = glob.glob(f"{folder}\**\*{ext}", recursive=recursive)
+            for file in files:
+                matches.extend(find(query, file))
 
-        files = glob.glob(f"{folder}\**\*{ext}", recursive=recursive)
-        matches = []
+def find(query, file):
+    f = open(file, "r")
+    content = f.read()
 
-        for file in files:
-            f = open(file, "r")
-            content = f.read()
+    if (callable(query)):
+        return query(content)
+    else:
+        matches = re.finditer(query, content, re.MULTILINE)
+        for matchNum, match in enumerate(matches, start=1):
+            print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
+    f.close()
 
-            if (callable(query)):
-                matches.extend(query(content))
-            else:
-                matches = re.finditer(query, content, re.MULTILINE)
-                for matchNum, match in enumerate(matches, start=1):
-                    print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
-            f.close()
+def save(results, name, location):
+    pass
 
-
-file_query('.','file_query', extensions=['.py'])
+findall('file_query', extensions=['.py'], folders=['*'])
